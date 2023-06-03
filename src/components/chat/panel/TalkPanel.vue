745ee26e6cfb4e5e20faa8de995bba50fe6e5d4f
politewang <politewang@tencent.com>
2023-06-03 18:54:27 +0800

    fix

--- src/components/chat/panel/TalkPanel.vue
+++ src/components/chat/panel/TalkPanel.vue
@@ -635,8 +635,7 @@ export default {
       DownImgBase64({url:avatarUrl})
       .then(({ code, data }) => {
           if (code !== 200) return
-          console.log('download ok, data:', data)
-          localStorage.setItem(path, "data:image/png;base64,"+data)          
+          localStorage.setItem(path, "data:image/png;base64,"+data.data)          
         })
         .catch((e) => {
           console.log('exception', e)
